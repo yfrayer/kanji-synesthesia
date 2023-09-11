e13ab32386bba17961fcd04ff2368460d7803be8
Yo Frayer <48837991+yfrayer@users.noreply.github.com>
2023-09-11 00:54:46 -0400

    Create kanjiyo.js

---
+++ kanjiyo.js
@@ -0,0 +1,619 @@
+var kanji = {
+  "ç›®çš„": {
+    "romanji": "mokuteki",
+    "meaning": "goal",
+    "color": "#e1e6ba"
+  },
+  "ç†ç”±": {
+    "romanji": "riyuu",
+    "meaning": "motive",
+    "color": "#7684c2"
+  },
+  "ã–ã‚ã‚ã": {
+    "romanji": "zawameki",
+    "meaning": "to fall out",
+    "color": "#76b4c2"
+  },
+  "åä»˜ã‘": {
+    "romanji": "nadzuke",
+    "meaning": "named",
+    "color": "#ad581f"
+  },
+  "æ™‚é–“": {
+    "romanji": "jikan",
+    "meaning": "time",
+    "color": "#2b4a75"
+  },
+  "å ´æ‰€": {
+    "romanji": "basho",
+    "meaning": "place",
+    "color": "#4bb387"
+  },
+  "ç´™": {
+    "romanji": "kami",
+    "meaning": "paper",
+    "color": "#cae9ed"
+  },
+  "é£›è¡Œæ©Ÿ": {
+    "romanji": "hikouki",
+    "meaning": "airplane",
+    "color": "#afcddb"
+  },
+  "ãµã‚‰ãµã‚‰": {
+    "romanji": "furafura",
+    "meaning": "unsteady",
+    "color": "#ccb8e0"
+  },
+  "é£›ã³è¾¼ã‚“ã§": {
+    "romanji": "tobikonde",
+    "meaning": "jump",
+    "color": "#1b7ec4"
+  },
+  "å¤‰ã‚ã‚‹": {
+    "romanji": "kawaru",
+    "meaning": "change",
+    "color": "#c4942d"
+  },
+  "é ã": {
+    "romanji": "touku",
+    "meaning": "distant",
+    "color": "#3c6ac7"
+  },
+  "èžã“ãˆãŸ": {
+    "romanji": "kikoeta",
+    "meaning": "heard",
+    "color": "#b86d23"
+  },
+  "é å ãˆ": {
+    "romanji": "touboe",
+    "meaning": "howling",
+    "color": "#241c45"
+  },
+  "æŒŸã‚“ã§": {
+    "romanji": "hasande",
+    "meaning": "across",
+    "color": "#c93c18"
+  },
+  "åº§ã£ãŸ": {
+    "romanji": "suwatta",
+    "meaning": "sitting",
+    "color": "#414d7d"
+  },
+  "å–‹ã£ã¦": {
+    "romanji": "shabatte",
+    "meaning": "spoke",
+    "color": "#438ba1"
+  },
+  "é»™ã£ã¦": {
+    "romanji": "damatte",
+    "meaning": "shut up",
+    "color": "#303891"
+  },
+  "æ›–æ˜§": {
+    "romanji": "aimai",
+    "meaning": "ambiguous",
+    "color": "#62d9cd"
+  },
+  "ä¸€ç·’": {
+    "romanji": "issho",
+    "meaning": "together",
+    "color": "#80522a"
+  },
+  "ãªãžã£ãŸ": {
+    "romanji": "nazotta",
+    "meaning": "traced",
+    "color": "#c9472c"
+  },
+  "ç¬‘é¡”": {
+    "romanji": "egao",
+    "meaning": "smile",
+    "color": "#e8e192"
+  },
+  "ãŸã‚æ¯": {
+    "romanji": "tameiki",
+    "meaning": "sigh",
+    "color": "#a592e8"
+  },
+  "è¿·å­": {
+    "romanji": "maigo",
+    "meaning": "lost",
+    "color": "#43c48e"
+  },
+  "å¤§ä¸ˆå¤«": {
+    "romanji": "daijoubu",
+    "meaning": "alright",
+    "color": "#8671e3"
+  },
+  "åƒ•": {
+    "romanji": "boku",
+    "meaning": "me",
+    "color": "#57a5d9"
+  },
+  "å›": {
+    "romanji": "kimi",
+    "meaning": "you",
+    "color": "#e8d241"
+  },
+  "çŸ¥ã£ã¦ã„ãŸ": {
+    "romanji": "shitteita",
+    "meaning": "knew",
+    "color": "#de6a12"
+  },
+  "æ°—ä»˜ã„ã¦ã„ãŸ": {
+    "romanji": "kidzuiteita",
+    "meaning": "realized",
+    "color": "#bf34a8"
+  },
+  "çµ‚ã‚ã‚‹": {
+    "romanji": "owaru",
+    "meaning": "end",
+    "color": "#6bc7f2"
+  },
+  "é­”æ³•": {
+    "romanji": "mahou",
+    "meaning": "magic",
+    "color": "#a329cc"
+  },
+  "æ˜¨æ—¥": {
+    "romanji": "kinou",
+    "meaning": "yesterday",
+    "color": "#d9ab38"
+  },
+  "ä¼¼ãŸ": {
+    "romanji": "nita",
+    "meaning": "alike",
+    "color": "#d47857"
+  },
+  "ç¹°ã‚Šè¿”ã—": {
+    "romanji": "kurikaeshi",
+    "meaning": "repetition",
+    "color": "#5791d4"
+  },
+  "æ™®é€š": {
+    "romanji": "futsuu",
+    "meaning": "normal",
+    "color": "#d4a457"
+  },
+  "å°‘ã—": {
+    "romanji": "sukoshi",
+    "meaning": "bit by bit",
+    "color": "#5560a1"
+  },
+  "ã“ã£ãã‚Š": {
+    "romanji": "kossori",
+    "meaning": "secretly",
+    "color": "#a165b5"
+  },
+  "å‰Šã‚‰ã‚ŒãŸ": {
+    "romanji": "kezurareta",
+    "meaning": "whittled",
+    "color": "#b89158"
+  },
+  "çž¬ã": {
+    "romanji": "mabataki",
+    "meaning": "blink",
+    "color": "#5cafbf"
+  },
+  "å‘ã“ã†": {
+    "romanji": "mukou",
+    "meaning": "beyond",
+    "color": "#363d96"
+  },
+  "ã„ã‚ã„ã‚": {
+    "romanji": "iroiro",
+    "meaning": "various",
+    "color": "#865ca1"
+  },
+  "ã„ãã¤ã‚‚": {
+    "romanji": "ikutsumo",
+    "meaning": "several",
+    "color": "#4cb586"
+  },
+  "è¦‹è½ã¨ã—": {
+    "romanji": "miotoshi",
+    "meaning": "oversight",
+    "color": "#c2b84e"
+  },
+  "ãµã‚Š": {
+    "romanji": "furi",
+    "meaning": "pretend",
+    "color": "#4ec253"
+  },
+  "è¿‘ãã¦": {
+    "romanji": "chikakute",
+    "meaning": "near",
+    "color": "#a3361d"
+  },
+  "è§¦ã‚Œãªã‹ã£ãŸ": {
+    "romanji": "furenakatta",
+    "meaning": "couldn't touch",
+    "color": "#bca3c4"
+  },
+  "å†—è«‡": {
+    "romanji": "joudan",
+    "meaning": "joke",
+    "color": "#4c5c6b"
+  },
+  "æ²ˆé»™": {
+    "romanji": "chinmoku",
+    "meaning": "silence",
+    "color": "#696f96"
+  },
+  "å¥¥": {
+    "romanji": "oku",
+    "meaning": "within",
+    "color": "#8ca3ab"
+  },
+  "éµ": {
+    "romanji": "kagi",
+    "meaning": "key",
+    "color": "#d99d3d"
+  },
+  "ä¸¸ã‚ãŸ": {
+    "romanji": "marumeta",
+    "meaning": "rolled up",
+    "color": "#677371"
+  },
+  "é¢å€’": {
+    "romanji": "mendou",
+    "meaning": "troublesome",
+    "color": "#96b370"
+  },
+  "æœ¬éŸ³": {
+    "romanji": "honne",
+    "meaning": "real intentions",
+    "color": "#9f70b3"
+  },
+  "ã¤ã£ã“ã‚“ã§": {
+    "romanji": "tsukkonde",
+    "meaning": "shoved into",
+    "color": "#5a6478"
+  },
+  "éš ã—ã¦ã„ãŸ": {
+    "romanji": "kakushiteita",
+    "meaning": "hiding",
+    "color": "#2a4482"
+  },
+  "å›ºã¾ã£ã¦": {
+    "romanji": "katamatte",
+    "meaning": "solidified",
+    "color": "#454b59"
+  },
+  "å¾…ã£ãŸ": {
+    "romanji": "matta",
+    "meaning": "waited",
+    "color": "#567fd1"
+  },
+  "ä¸¦ã‚“ã§": {
+    "romanji": "narande",
+    "meaning": "alongside",
+    "color": "#f06a48"
+  },
+  "æ¥½ã—ãã¦": {
+    "romanji": "tanoshikute",
+    "meaning": "fun",
+    "color": "#f0c648"
+  },
+  "ã‚ã¾ã‚Š": {
+    "romanji": "amari",
+    "meaning": "somehow",
+    "color": "#b8d177"
+  },
+  "çœ©ã—": {
+    "romanji": "mabushi",
+    "meaning": "glare",
+    "color": "#5cf2f7"
+  },
+  "æƒ³åƒ": {
+    "romanji": "souzou",
+    "meaning": "envision",
+    "color": "#8756b3"
+  },
+  "æœªæ¥": {
+    "romanji": "mirai",
+    "meaning": "future",
+    "color": "#36a7c9"
+  },
+  "ç«‹ã£ã¦": {
+    "romanji": "tatte",
+    "meaning": "standing",
+    "color": "#45478a"
+  },
+  "ç›¸å¤‰ã‚ã‚‰ãš": {
+    "romanji": "aikawarazu",
+    "meaning": "as usual",
+    "color": "#614345"
+  },
+  "æ€ªæˆ‘": {
+    "romanji": "kega",
+    "meaning": "injuries",
+    "color": "#9c3294"
+  },
+  "æŽŒ": {
+    "romanji": "tenohira",
+    "meaning": "palm",
+    "color": "#afcf46"
+  },
+  "å‹•ã‹": {
+    "romanji": "ugoka",
+    "meaning": "move",
+    "color": "#555ed9"
+  },
+  "æ™¯è‰²": {
+    "romanji": "keshiki",
+    "meaning": "scenery",
+    "color": "#6edbaa"
+  },
+  
+  
+  "å¤¢": {
+    "romanji": "yume",
+    "meaning": "dream",
+    "color": "#94c3e0"
+  },
+  "é›†": {
+    "romanji": "atsu",
+    "meaning": "gather",
+    "color": "#e64225"
+  },
+  "æœ": {
+    "romanji": "saga",
+    "meaning": "search",
+    "color": "#5a77c7"
+  },
+  "ç‰©": {
+    "romanji": "mono",
+    "meaning": "thing",
+    "color": "#828282"
+  },
+  "æŽ¢": {
+    "romanji": "saga",
+    "meaning": "search",
+    "color": "#5a77c7"
+  },
+  "è¡Œ": {
+    "romanji": "yuku",
+    "meaning": "going",
+    "color": "#debd3c"
+  },
+  "ç¾…é‡ç›¤": {
+    "romanji": "rashinban",
+    "meaning": "compass",
+    "color": "#9ba0bf"
+  },
+  "æ¸‹æ»ž": {
+    "romanji": "juutai",
+    "meaning": "delay",
+    "color": "#798bad"
+  },
+  "ç†±": {
+    "romanji": "netsu",
+    "meaning": "passion",
+    "color": "#cf273e"
+  },
+  "èˆµ": {
+    "romanji": "kaji",
+    "meaning": "helm",
+    "color": "#ab663c"
+  },
+  "å®": {
+    "romanji": "takara",
+    "meaning": "treasure",
+    "color": "#c4882d"
+  },
+  "åœ°å›³": {
+    "romanji": "chizu",
+    "meaning": "map",
+    "color": "#cfaa61"
+  },
+  "ç¢º": {
+    "romanji": "tashi",
+    "meaning": "confirmed",
+    "color": "#d49b33"
+  },
+  "ä¼èª¬": {
+    "romanji": "densetsu",
+    "meaning": "legend",
+    "color": "#afb849"
+  },
+  "å€‹äººçš„": {
+    "romanji": "kojinteki",
+    "meaning": "personal",
+    "color": "#43bf94"
+  },
+  "åµ": {
+    "romanji": "arashi",
+    "meaning": "storm",
+    "color": "#498db8"
+  },
+  "èª°": {
+    "romanji": "dare",
+    "meaning": "who",
+    "color": "#cc3b3b"
+  },
+  "ä¹—": {
+    "romanji": "no",
+    "meaning": "power",
+    "color": "#cc3b3b"
+  },
+  "æ€": {
+    "romanji": "omo",
+    "meaning": "think",
+    "color": "#979974"
+  },
+  "éŽ": {
+    "romanji": "su",
+    "meaning": "too much",
+    "color": "#70c2a8"
+  },
+  "å—": {
+    "romanji": "u",
+    "meaning": "accept",
+    "color": "#70c2a8"
+  },
+  "ä¿¡": {
+    "romanji": "shin",
+    "meaning": "believe",
+    "color": "#6b81c9"
+  },
+  "è‚©": {
+    "romanji": "kata",
+    "meaning": "shoulder",
+    "color": "#d14f32"
+  },
+  "æŠ¼": {
+    "romanji": "o",
+    "meaning": "push",
+    "color": "#d1be32"
+  },
+  "æ­©": {
+    "romanji": "ppo",
+    "meaning": "step",
+    "color": "#86d158"
+  },
+  "ä¼š": {
+    "romanji": "a",
+    "meaning": "meet",
+    "color": "#cf5751"
+  },
+  "è©±": {
+    "romanji": "tell",
+    "meaning": "hana",
+    "color": "#d4a339"
+  },
+  "å‡ºæ¥": {
+    "romanji": "deki",
+    "meaning": "done",
+    "color": "#47a6cc"
+  },
+  "è‡ªæ„è­˜": {
+    "romanji": "jiishiki ",
+    "meaning": "self conscious",
+    "color": "#85b8d6"
+  },
+  "éŽå‰°": {
+    "romanji": "kajou",
+    "meaning": "excess",
+    "color": "#8462b5"
+  },
+  "å¤œ": {
+    "romanji": "yoru",
+    "meaning": "night",
+    "color": "#101e42"
+  },
+  "å®ç®±": {
+    "romanji": "takarabako",
+    "meaning": "treasure",
+    "color": "#b57f2f"
+  },
+  "é ƒ": {
+    "romanji": "koro",
+    "meaning": "time",
+    "color": "#5e9acc"
+  },
+  "é ­": {
+    "romanji": "atama",
+    "meaning": "head",
+    "color": "#d9573d"
+  },
+  "æµ®": {
+    "romanji": "u",
+    "meaning": "float",
+    "color": "#79b9d9"
+  },
+  "å ´æ‰€": {
+    "romanji": "basho",
+    "meaning": "place",
+    "color": "#44c2a3"
+  },
+  "çŸ¥": {
+    "romanji": "shi",
+    "meaning": "know",
+    "color": "#6a63ab"
+  },
+  "è² ": {
+    "romanji": "ma",
+    "meaning": "defeat",
+    "color": "#662d5c"
+  },
+  "å§”": {
+    "romanji": "maka",
+    "meaning": "leave up to",
+    "color": "#6c7394"
+  },
+  "ä¸–ç•Œ": {
+    "romanji": "sekai",
+    "meaning": "world",
+    "color": "#3e62c7"
+  },
+  "å¤‰": {
+    "romanji": "ka",
+    "meaning": "change",
+    "color": "#b0c73e"
+  },
+  "é€£": {
+    "romanji": "tsu",
+    "meaning": "take along",
+    "color": "#c7703e"
+  },
+  "æ­Œ": {
+    "romanji": "uta",
+    "meaning": "song",
+    "color": "#d6cd27"
+  },
+  "å”„": {
+    "romanji": "uta",
+    "meaning": "sing",
+    "color": "#d6cd27"
+  },
+  "çœŸä¼¼": {
+    "romanji": "mane",
+    "meaning": "imitate",
+    "color": "#0c91cf"
+  },
+  "ç§˜å¯†": {
+    "romanji": "himitsu",
+    "meaning": "secret",
+    "color": "#3c14a8"
+  },
+  "ä¸Šæ‰‹": {
+    "romanji": "jouzu",
+    "meaning": "skillful",
+    "color": "#3545b0"
+  },
+  "èž": {
+    "romanji": "ki",
+    "meaning": "hear",
+    "color": "#4c7fc7"
+  },
+  "æ¯": {
+    "romanji": "iki",
+    "meaning": "breath",
+    "color": "#6ea3d4"
+  },
+  "æœ¬å½“": {
+    "romanji": "hontou",
+    "meaning": "real",
+    "color": "#c4690e"
+  },
+  "æ™‚ä»£": {
+    "romanji": "jidai",
+    "meaning": "era",
+    "color": "#0eb8c4"
+  },
+  "æˆ»": {
+    "romanji": "modo",
+    "meaning": "return",
+    "color": "#238735"
+  },
+  "æ¶™": {
+    "romanji": "namida",
+    "meaning": "tears",
+    "color": "#3aa8c9"
+  },
+  "è¿½": {
+    "romanji": "o",
+    "meaning": "chase",
+    "color": "#3a84c9"
+  }
+}
